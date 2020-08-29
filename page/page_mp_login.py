6867659ab152ac409753f68df4497f0dc81f2dde
2761594771 <2761594771@qq.com>
2020-08-30 00:16:09 +0800

    修改验证方法、点击登录按钮方法

--- page/page_mp_login.py
+++ page/page_mp_login.py
@@ -8,12 +8,12 @@ class PageMpLogin(Base):
     def page_input_username(self):
         self.page.mp_username
     # 输入验证码方法 封装
-    def page_input_code(self):
-        pass
+    def page_input_code(self,code):
+        self.base_input(code)
 
     # 点击 登录按钮封装
-    def page_click_login_btn(self):
-        pass
+    def page_click_login_btn(self,loc):
+        self.base_click(loc)
 
     # 获取昵称 封装
     def page_get_nickname(self):
